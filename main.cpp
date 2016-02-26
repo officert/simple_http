dc726e9d4a896e97f8aa4231f76b7acbeaae0466
Tim Officer <timothyofficer@gmail.com>
2016-02-26 16:32:49 -0500

    helpers for parsing cli args

--- main.cpp
+++ main.cpp
@@ -4,10 +4,26 @@
 
 using namespace std;
 
-int main() {
+//function prototypes
+// void validate_args(int argc, char* argv[]);
+// void display_help();
+
+int main(int argc, char* argv[]) {
 	Server server;
 
 	server.initServer();
 
 	return 0;
 }
+
+// void validate_args(int argc, char* argv[]) {
+// 	cout << "argc = " << argc << endl;
+//    for(int i = 0; i < argc; i++)
+//       cout << "argv[" << i << "] = " << argv[i] << endl;
+// }
+//
+// void display_help() {
+// 	cout << "--------------------------------" << endl;
+// 	cout << "Server Util" << endl;
+// 	cout << "--------------------------------" << endl;
+// }
